6f709c0d4ec8bf788b72a28c8c8de9511a103259
Derek Dela Cruz <derekdelacruz43@gmail.com>
2024-02-13 14:16:25 -0800

    final version (again)

--- rgbpong.py
+++ rgbpong.py
@@ -146,7 +146,7 @@ pygame.init()
 clock = pygame.time.Clock()
 
 screen_width = 1280
-screen_height = 800
+screen_height = 960
 screen = pygame.display.set_mode((screen_width, screen_height))
 pygame.display.set_caption("RGBPong")
 
